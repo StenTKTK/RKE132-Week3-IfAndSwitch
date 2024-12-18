ca1011265606617d501b226865de48e8b7479c39
Sten Eric Persidski <sten.eric.persidski@tktk.ee>
2024-12-18 16:16:56 +0200

    add week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+Console.WriteLine("Enter a number:!");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris või paaritu
+
+int results = userNum % 2;
+
+if(results != 0) //!= - ei ole
+{
+    Console.WriteLine("User number is odd.");
+} else
+{
+    Console.WriteLine("User number is even");
+}
+
+
+
+
+
+
+
+
+
+
